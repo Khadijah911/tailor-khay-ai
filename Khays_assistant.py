31f7721453589fb489389099e40e8934886d1584
Khadijah911 <sakakhadijah91@gmail.com>
2026-07-11 12:52:09 +0100

    Add tool calling with calendar availability checker

--- Khays_assistant.py
+++ Khays_assistant.py
@@ -12,8 +12,10 @@ from langgraph.graph.message import add_messages
 from langchain_core.prompts import MessagesPlaceholder
 from langgraph.checkpoint.memory import MemorySaver
 from langchain_openai import ChatOpenAI
-import os
 
+from langchain_openai import ChatOpenAI
+from langgraph.prebuilt import ToolNode,tools_condition
+import os
 
 api_key = os.getenv("OPENAI_API_KEY")
 model = ChatOpenAI(
@@ -21,15 +23,30 @@ model = ChatOpenAI(
     api_key=api_key
 )
 
+
 class GraphState(TypedDict):
   ai_message : str
   human_message : str
   intent : str
-
+  should_continue : str
   next_action : str
   messages : Annotated[list,add_messages]
 
 memory=MemorySaver()
+
+calendar = {
+    "2026-07-10": "available",
+    "2026-07-12": "available",
+    "2026-07-14": "booked",
+    "2026-07-16": "booked",
+    "2026-07-18": "available",
+    "2026-07-20": "available",
+    "2026-07-22": "available",
+    "2026-07-24": "booked",
+    "2026-07-26": "booked",
+    "2026-07-30": "booked",
+}
+
 def collect_customer_message(state):
   user_inquiry =  input('enter you inquiry :')
   return {
@@ -38,6 +55,22 @@ def collect_customer_message(state):
       'messages' : [HumanMessage(content = user_inquiry)]
 
   }
+
+from langchain_core.tools import tool
+
+
+
+@tool
+def check_calendar(date : str):
+  """Checks the availability status for a given date in the calendar."""
+  print(f"Tool called with: {date}")  
+  if date in calendar:
+    return { 'status' : calendar[date]}
+  else:
+    return{'status' : 'not available'}
+
+model_with_tools = model.bind_tools([check_calendar])
+tool_node=ToolNode([check_calendar])
 
 def khay_assistant(state):
 
@@ -55,13 +88,14 @@ def khay_assistant(state):
      {'messages' : state['messages']}
      )
 
-  response =model.invoke(message)
+
+
+
+  response =model_with_tools.invoke(message)
   print(response.content)
   return {
       'ai_message' : response.content,
-      'messages' : [AIMessage(content = response.content)
-
-  ]
+      'messages' : [ response]
 
   }
 
@@ -101,7 +135,7 @@ def pricing (state):
   }
 
 def should_continue(state):
-  if 'bye' in state['human_message'].lower().split():
+  if 'bye' in state['human_message'].lower().split(' '):
 
       return {
           'next_action': 'stop'
@@ -120,7 +154,8 @@ graph.add_node('collect_customer_message', collect_customer_message)
 graph.add_node('book_appointment',book_appointment)
 graph.add_node('pricing',pricing)
 graph.add_node('intent_router',intent_router)
-graph.add_node('should_continue', should_continue)
+graph.add_node('continue_router', should_continue)
+graph.add_node('tools',tool_node)
 graph.add_edge(START,'collect_customer_message')
 graph.add_edge('collect_customer_message' , 'intent_router')
 graph.add_conditional_edges ('intent_router',
@@ -129,15 +164,19 @@ graph.add_conditional_edges ('intent_router',
                 'appointment': 'book_appointment',
                 'general' : 'khay_assistant'
                  })
-graph.add_conditional_edges('should_continue',
+graph.add_conditional_edges( 'khay_assistant', tools_condition)
+graph.add_edge('tools','khay_assistant')
+graph.add_edge('khay_assistant','continue_router')
+graph.add_conditional_edges('continue_router',
                             lambda state : state['next_action'],
                             {'continue' : 'collect_customer_message',
                              'stop' : END}
                             )
 
-graph.add_edge('book_appointment', 'should_continue')
-graph.add_edge('pricing', 'should_continue')
-graph.add_edge('khay_assistant', 'should_continue')
+
+graph.add_edge('book_appointment', 'continue_router')
+graph.add_edge('pricing', 'continue_router')
+
 app=graph.compile(checkpointer=memory)
 app.invoke({ },
            config = {
@@ -145,6 +184,4 @@ app.invoke({ },
                    'thread_id' : 'customer_001'
                }
            })
-
-
 
